7bfa2cc570340b747c2e315017ccfed2abb71ee8
Robert Witchell <robertwitchell@gmail.com>
2025-10-13 18:32:10 +1100

    feat(app): upgrade expo to v54

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -3,7 +3,7 @@ const withStorybook = require("@storybook/react-native/metro/withStorybook")
 // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
 const { getDefaultConfig } = require('expo/metro-config')
 // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
-const path = require('path')
+// const path = require('path')
 // eslint-disable-next-line @typescript-eslint/no-require-imports,no-undef
 const { withNativeWind } = require('nativewind/metro');
 
@@ -11,18 +11,18 @@ const { withNativeWind } = require('nativewind/metro');
 // eslint-disable-next-line no-undef
 const projectRoot = __dirname
 // This can be replaced with `find-yarn-workspace-root`
-const monorepoRoot = path.resolve(projectRoot, '../..')
+// const monorepoRoot = path.resolve(projectRoot, '../..')
 
 let config = getDefaultConfig(projectRoot)
 
 // 1. Watch all files within the monorepo
-config.watchFolders = [...config.watchFolders, monorepoRoot]
+// config.watchFolders = [...config.watchFolders, monorepoRoot]
 
 // 2. Let Metro know where to resolve packages and in what order
-config.resolver.nodeModulesPaths = [
-    path.resolve(projectRoot, 'node_modules'),
-    path.resolve(monorepoRoot, 'node_modules'),
-]
+// config.resolver.nodeModulesPaths = [
+//     path.resolve(projectRoot, 'node_modules'),
+//     path.resolve(monorepoRoot, 'node_modules'),
+// ]
 
 config = withNativeWind(config, {input: './global.css'})
 config = withStorybook(config)
